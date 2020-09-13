fb291cc105c4033d1f1b79a663708a4f2265bfe3
plinerdaria <daria.pliner@gmail.com>
2020-09-13 11:55:31 +0300

    have added fail switch.js

---
+++ scripts/switch.js
@@ -0,0 +1,59 @@
+// let browser = prompt("what is your browser?", "Edge");
+// switch (browser) {
+//   case "Edge":
+//     alert("You've got the Edge!");
+//     break;
+
+//   case "Chrome":
+//   case "Firefox":
+//   case "Safari":
+//   case "Opera":
+//     alert("Okay we support these browsers too");
+//     break;
+
+//   default:
+//     alert("We hope that this page looks ok!");
+// }
+// let browser = `Opera`;
+// if (browser === "Edge") {
+//   console.log("You've got the Edge!");
+// } else if (
+//   browser === "Chrome" ||
+//   browser === "Firefox" ||
+//   browser === "Safari" ||
+//   browser === "Opera"
+// ) {
+//   console.log("Okay we support these browsers too");
+// } else {
+//   console.log("We hope that this page looks ok!");
+// }
+
+// const number = +prompt('Введите число между 0 и 3', '');
+
+// if (number === 0) {
+//   alert('Вы ввели число 0');
+// }
+
+// if (number === 1) {
+//   alert('Вы ввели число 1');
+// }
+
+// if (number === 2 || number === 3) {
+//   alert('Вы ввели число 2, а может и 3');
+// }
+
+// let number = 0;
+// switch (number) {
+//   case 0:
+//     console.log("Вы ввели число 0");
+//     break;
+//   case 1:
+//     console.log("Вы ввели число 1");
+//     break;
+//   case 2:
+//   case 3:
+//     console.log("Вы ввели число 2, а может и 3");
+//     break;
+//   default:
+//     console.log("Попытайтесь еще");
+// }
