8424ca1b753fa2d79e5a7c848fa27c0f671bb895
plinerdaria <daria.pliner@gmail.com>
2020-09-18 10:17:27 +0300

    have written one more task

--- scripts/objects.js
+++ scripts/objects.js
@@ -127,3 +127,43 @@
 // console.log(calculator.sum());
 // console.log(calculator.mul());
 // console.log(calculator);
+
+// Это ladder (лестница) – объект, который позволяет подниматься вверх и спускаться:
+
+// let ladder = {
+//   step: 0,
+//   up() {
+//     this.step++;
+//   },
+//   down() {
+//     this.step--;
+//   },
+//   showStep: function() { // показывает текущую ступеньку
+//     alert( this.step );
+//   }
+// };
+// Теперь, если нам нужно сделать несколько последовательных вызовов, мы можем выполнить это так:
+
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 1
+// Измените код методов up, down и showStep таким образом, чтобы их вызов можно было сделать по цепочке, например так:
+
+// let ladder = {
+//   step: 0,
+//   up() {
+//     this.step++;
+//     return ladder;
+//   },
+//   down() {
+//     this.step--;
+//     return ladder;
+//   },
+//   showStep: function () {
+//     // показывает текущую ступеньку
+//     console.log(this.step);
+//   },
+// };
+
+// ladder.up().up().down().showStep(); // 1
